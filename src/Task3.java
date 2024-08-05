8bcdb5860fc249658229cd100cd268f1937fee25
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-05 22:35:43 +0300

    task 3

---
+++ src/Task3.java
@@ -0,0 +1,13 @@
+public class Task3 {
+    static public void printColor() {
+        int value = 33;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
