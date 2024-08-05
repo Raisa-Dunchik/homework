a6242bd5d798bd7a07f316593f7688e1a54988f4
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-05 22:10:28 +0300

    task 1

---
+++ src/Task1.java
@@ -0,0 +1,7 @@
+public class Task1 {
+    static public void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
