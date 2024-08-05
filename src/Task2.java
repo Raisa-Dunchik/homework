cd59a6d39c0bbbd9ff5918da000c187980d06c85
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-05 22:26:40 +0300

    task 2

---
+++ src/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+    static public void checkSumSign() {
+        int a = -10;
+        int b = 5;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+}
