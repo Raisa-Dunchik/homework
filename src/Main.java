a0cc76de59e5aeed09d2d117376de5d4a049425c
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-06 19:49:33 +0300

    task 9

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        int year = 2024;
-        boolean isLeap = Task9.isLeapYear(year);
-        if (isLeap) {
-            System.out.println(year + " is a leap year ");
-        } else {
-            System.out.println(year + " is not a leap year ");
-        }
+  public class Main  {
+  public static void main(String[] args) {
+  System.out.print("Массив после замены: ");
+  int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+  for (int num : arr) {
+     System.out.println(num + "");
     }
 }
+}
+
+
 
