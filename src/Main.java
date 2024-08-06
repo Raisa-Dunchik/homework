00e71d6211f845f45ecf32a30a5c1053308b955c
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-06 19:49:33 +0300

    task 5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static public void main(String[] args) {
-        int num1 = -5;
-        int num2 = 0;
-        int num3 = 10;
-        System.out.println(num1 + " отрицательное? " + Task7.isNegative(num1));
-        System.out.println(num2 + " отрицательное? " + Task7.isNegative(num2));
-        System.out.println(num3 + " отрицательное? " + Task7.isNegative(num3));
+    public static void main(String[] args) {
+        Task8.printStringMultipleTimes("Hello, World", 5);
     }
 }
---
+++ src/Task8.java
@@ -0,0 +1,7 @@
+public class Task8 {
+    public static void printStringMultipleTimes(String str, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(str);
+        }
+    }
+}
