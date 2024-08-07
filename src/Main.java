a2c9fd13e541253c99789d859bc0bde25c42aa45
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-07 15:40:05 +0300

    task 10

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-  public class Main  {
-  public static void main(String[] args) {
-  System.out.print("Массив после замены: ");
+  public class Main {
+  public static void main (String[] args)  {
+  System.out.print("Массив после замены: Task10 ");
   int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
   for (int num : arr) {
-     System.out.println(num + "");
+     System.out.println (num + "");
     }
 }
 }
