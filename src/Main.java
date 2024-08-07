bda13760ce1caad027f0a57897bb152b68fe0d07
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-07 15:40:05 +0300

    task 12

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-  public static void main(String[] args) {
-   int[] array = new int[100];
-      for (int i = 0; i < array.length; i++) {
-          array[i] = i + 1;
-      System.out.print(array[i] + " ");
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            System.out.println(array[i] + "");
+            }
+        }
     }
-  }
-  }
-
-
+}
--- src/Task10.java
+++ src/Task10.java
@@ -1,7 +1,7 @@
 
 public class Task10 {
     public static void main(String[] args) {
-        int [] arr = new int [1];
-        int [] test1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr = new int[1];
+        int[] test1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
     }
 }
