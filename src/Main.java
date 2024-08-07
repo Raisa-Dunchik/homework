5f0a38f69d0c80e5ab41c0d8dc468796d66afbb8
Raisa Duchik <145raisa.ro9@gmail.com>
2024-08-07 15:40:05 +0300

    task 10

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-  public class Main {
-  public static void main (String[] args)  {
-  System.out.print("Массив после замены: Task10 ");
-  int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-  for (int num : arr) {
-     System.out.println (num + "");
+public class Main {
+  public static void main(String[] args) {
+    int [] arr = new int [1];
+    int [] test1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+      System.out.println(Arrays.toString(test1));
     }
-}
-}
+  }
+
+
 
 
 
